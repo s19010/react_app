8a21fa596c340c5506e9484fe29130cc063c8273
s19010 <s19010@std.it-college.ac.jp>
2019-12-17 15:58:54 +0900

    挨拶を表示

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
+const Greeting = props => (
+  <h1>
+    <messages />
+  </h1>
+)
+
+const messages = [
+  '今日も頑張りましょう。',
+  'こんにちは。',
+  '朗らかな日ですね。'
+]
+
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
+
   render () {
-    return <div></div>
+    return (
+      <div>
+        <messages />
+      </div>
+    )
   }
 }
 
