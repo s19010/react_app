e0bba18c4a16c6468b4df67790e0f35d57d41d3a
s19010 <s19010@std.it-college.ac.jp>
2019-12-17 15:08:17 +0900

    ハローワールド

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-const Title = props => {
-  return <h1>{props.text}</h1>
+const Hello = props => {
+  return <h1>Hello World!</h1>
 }
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <h1>Hello World!</h1>
+      <div>
+          <Hello />
       </div>
     )
   }
