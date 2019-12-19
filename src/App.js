07f11b1bd05c756898b3003e39e7852aab7fb95d
s19010 <s19010@std.it-college.ac.jp>
2019-12-19 16:02:55 +0900

    ライフサイクルの確認

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react'
-
-class App extends React.Component {
+import React, { Component } from 'react'
+class App extends Component {
+  // マウント
   constructor (props) {
     super(props)
-    this.state = {}
+    console.log('constructor')
+  }
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+  // 更新
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps')
+  }
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate')
+    return true
+  }
+  componentWillUpdate () {
+    console.log('componentWillUpdate')
+  }
+  componentDidUpdate () {
+    console.log('componentDidUpdate')
+  }
+  // アンマウント
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
   }
   render () {
-    return <div></div>
+    console.log('render')
+    const setStateHandler = e => {
+      console.log('* call setState()')
+      this.setState({ r: Math.random() })
+    }
+    return (
+      <div>
+        <button onClick={setStateHandler}>setState</button>
+      </div>
+    )
   }
 }
 
